500db8e88218416c48a634a71ecab41e988590c4
prateepcs4 <prateepcs4@gmail.com>
2017-04-08 01:22:22 +0530

    First commit for generator

--- model.py
+++ model.py
@@ -6,7 +6,7 @@ from keras.layers.core import Activation
 from keras.activations import relu, tanh
 import numpy as np
 
-batch_size = 5
+batch_size = 1
 in_sequence_len = 3
 out_sequence_len = 3
 init_res_gen = 512
@@ -21,19 +21,19 @@ def generator():
     network.add(BatchNormalization())
     network.add(Activation('relu'))
     network.add(UpSampling3D((in_sequence_len, 16, 16)))
-    network.add(Conv3D(init_res_gen/4, kernel_size=3, activation='relu', padding='same'))
-    network.add(BatchNormalization())
-    network.add(Activation('relu'))
-    network.add(UpSampling3D((in_sequence_len, 32, 32)))
-    network.add(Conv3D(init_res_gen / 8, kernel_size=3, activation='relu', padding='same'))
-    network.add(BatchNormalization())
-    network.add(Activation('relu'))
-    network.add(UpSampling3D((in_sequence_len, 64, 64)))
-    network.add(Conv3D(1, kernel_size=3, activation='relu', padding='same'))
-    network.add(BatchNormalization())
-    network.add(Activation('tanh'))
+    # network.add(Conv3D(init_res_gen/4, kernel_size=3, activation='relu', padding='same'))
+    # network.add(BatchNormalization())
+    # network.add(Activation('relu'))
+    # network.add(UpSampling3D((in_sequence_len, 32, 32)))
+    # network.add(Conv3D(init_res_gen / 8, kernel_size=3, activation='relu', padding='same'))
+    # network.add(BatchNormalization())
+    # network.add(Activation('relu'))
+    # network.add(UpSampling3D((in_sequence_len, 64, 64)))
+    # network.add(Conv3D(1, kernel_size=3, activation='relu', padding='same'))
+    # network.add(BatchNormalization())
+    # network.add(Activation('tanh'))
 
-    input = np.random.rand(5, 200)
+    input = np.random.rand(batch_size, 200)
     output = network.predict(input, batch_size=batch_size, verbose=0)
     print np.shape(output)
 
