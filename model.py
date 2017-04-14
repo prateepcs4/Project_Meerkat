2d2b55e3b9b6d12f9ea26e27b9ea699e4e89021e
prateepcs4 <prateepcs4@gmail.com>
2017-04-14 17:32:50 +0530

    First commit for discriminator_32x32

--- model.py
+++ model.py
@@ -1,7 +1,7 @@
 import numpy as np
-from keras.layers import Dense, Reshape
+from keras.layers import Dense, Reshape, TimeDistributed
 from keras.layers.convolutional import Conv3D, UpSampling3D
-from keras.layers.core import Activation
+from keras.layers.core import Activation, Flatten
 from keras.layers.normalization import BatchNormalization
 from keras.models import Sequential
 
@@ -12,15 +12,24 @@ init_res_gen = 512
 
 def generator_32x32():
     network = Sequential()
-    network.add(Conv3D(16, kernel_size=3, activation='relu', padding='same', input_shape=(3, 64, 64, 3)))
+    network.add(Conv3D(16, kernel_size=3, activation='relu', padding='same', input_shape=(3, 32, 32, 3)))
     network.add(Conv3D(32, kernel_size=3, activation='relu', padding='same'))
     network.add(Conv3D(64, kernel_size=3, activation='relu', padding='same'))
     network.add(Conv3D(32, kernel_size=3, activation='relu', padding='same'))
     network.add(Conv3D(16, kernel_size=3, activation='relu', padding='same'))
     network.add(Conv3D(3, kernel_size=3, activation='tanh', padding='same'))
 
-    input = np.random.rand(batch_size, 3, 64, 64, 3)
-    output = network.predict(input, batch_size=batch_size, verbose=0)
-    print np.shape(output)
-#
-generator_32x32()
+    return network
+
+def discriminator_32x32():
+    network = Sequential()
+    network.add(TimeDistributed(Flatten()))
+
+    return network
+
+G = generator_32x32()
+D = G.add(discriminator_32x32())
+
+input = np.random.rand(batch_size, 3, 32, 32, 3)
+output = D.predict(input, batch_size=batch_size, verbose=0)
+print np.shape(output)
