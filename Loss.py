d20b1b306ad0db839ce98d1aa10b04fa02d02581
prateepcs4 <prateepcs4@gmail.com>
2017-04-21 03:16:28 +0530

    Experimental commit for loss functions

--- Loss.py
+++ Loss.py
@@ -18,13 +18,21 @@ def l1_loss(gen_frames, gt_frames, l_p):
     return lp
 
 
-# Utility function for fast computation of patches from a tensor
-def patchify(input, ksize, stride):
-    patches = tf.extract_image_patches(input, ksizes=[1, ksize, ksize, 1], strides=[1, stride, stride, 1],
+# Utility function for fast computation of patches from a 4D tensor (batchsize x H x W x channels) (supports batch mode)
+def patchify(input, ksize, stride, channels):
+    patches = tf.extract_image_patches(input, ksizes=[1, ksize, ksize, 1], strides=stride,
                                                rates=[1, 1, 1, 1], padding='VALID')
-    patches = tf.reshape(patches, [-1, ksize, ksize, 1])
+    patches = tf.reshape(patches, [-1, ksize, ksize, channels])
     return patches
 
+# Utility funtion for calculating the correlation score between a base matrix and an input template
+def xcorr(base, patch):
+    base_r_channel = tf.expand_dims(base[:, :, :, 0], -1)
+    patch_r_channel = tf.expand_dims(tf.expand_dims(patch[0, :, :, 0], -1), -1)
+    corr = tf.reduce_mean(tf.nn.conv2d(base_r_channel, patch_r_channel, [1, 1, 1, 1], padding='VALID'))
+    print sess.run(corr)
+
+stride = 2
 patch_size_cur = 2
 patch_size_prev = patch_size_cur + 2
 
@@ -41,14 +49,30 @@ def cross_corr_loss(in_frames, gen_frames, gt_frames, alpha):
         for frame_index in xrange(n_frames):
             prev_frame = last_in_frame if frame_index == 0 else gen_frames[batch_index][frame_index-1]
             cur_frame = gen_frames[batch_index][frame_index]
-            # print prev_frame.get_shape()
-            padded_prev_frame = tf.pad(prev_frame, pad, "CONSTANT")
+            padded_prev_frame = tf.expand_dims(tf.pad(prev_frame, pad, "CONSTANT"), 0)
             # Extract patches from the cur_frame
-            cur_frame = cur_frame[:,:,0]
-            cur_frame = tf.expand_dims(tf.expand_dims(cur_frame, 0), -1)
-            patch_cur_frames = patchify(cur_frame, patch_size_cur, 2)
+            cur_frame = tf.expand_dims(cur_frame, 0)
+            patches_cur_frame = patchify(cur_frame, patch_size_cur, [1, stride, stride, 1], 3)
+            n_patches, patch_height, _, _ = patches_cur_frame.get_shape().as_list()
+            # Extract patches from the previous frame for mapping
+            patches_prev_frame = patchify(padded_prev_frame, patch_size_prev, [1, stride-1, stride-1, 1], 3)
+            # Iterate over the patches
+            dim_patch_grid = n_rows/patch_height
+            for cur_patch_index in xrange(n_patches):
+                left_x = 2 * (cur_patch_index / dim_patch_grid)
+                left_y = 2 * (cur_patch_index % dim_patch_grid)
+                cur_prev_patch = tf.slice(padded_prev_frame, [0, left_x, left_y, 0], [1, patch_size_prev,
+                                                                                      patch_size_prev, 3])
+                score = xcorr(cur_prev_patch, tf.expand_dims(patches_cur_frame[cur_patch_index], 0))
 
+# Tester for patchify
+def patch_test(input):
+    input = tf.pad(input, [[0,0], [1, 1], [1, 1], [0, 0]], "CONSTANT")
+    patch = patchify(input, 2, 2, 3)
+    print patch.get_shape()
+    print sess.run(patch[1,:,:,1])
 
+# patch_test(tf.ones([1, 10, 10, 3]))
 
 # Tester for loss functions
 
@@ -62,5 +86,5 @@ gen_frames = tf.ones([BATCH_SIZE, 3, 32, 32, 3])
 gt_frames = tf.ones([BATCH_SIZE, 3, 32, 32, 3])
 res_tru = 0
 
-sess.run(cross_corr_loss(in_frames, gen_frames, gt_frames, 1))
+cross_corr_loss(in_frames, gen_frames, gt_frames, 1)
 # assert res == res_tru, 'Failed'
